42e34a7ec3a5cea77d89c85c618004c1611d8387
al177 <al177@users.noreply.github.com>
2022-07-28 14:09:51 -0500

    Increase default extruder current

--- Firmware/variants/1_75mm_MK3S-EINSy10a-E3Dv6full.h
+++ Firmware/variants/1_75mm_MK3S-EINSy10a-E3Dv6full.h
@@ -284,7 +284,7 @@
 
 //new settings is possible for vsense = 1, running current value > 31 set vsense to zero and shift both currents by 1 bit right (Z axis only)
 #define TMC2130_CURRENTS_H {26, 29, 37, 30}  // default holding currents for all axes
-#define TMC2130_CURRENTS_R {26, 29, 37, 30}  // default running currents for all axes
+#define TMC2130_CURRENTS_R {26, 29, 37, 38}  // default running currents for all axes
 #define TMC2130_CURRENTS_FARM 36             // E 805 mA peak for ECool/farm mode
 //#define TMC2130_CURRENTS_H {16, 20, 35, 30}  // default holding currents for all axes
 //#define TMC2130_CURRENTS_R {16, 20, 35, 30}  // default running currents for all axes
